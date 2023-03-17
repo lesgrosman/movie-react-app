349aa31a6cee0198ae7a2be4144b3ff9a5fbe513
lesgrosman <andreyagasov@gmail.com>
2023-03-17 10:49:09 +0100

    fix and features

--- src/components/Layout/ProfileLayout.tsx
+++ src/components/Layout/ProfileLayout.tsx
@@ -9,7 +9,7 @@ const ProfileLayout = ({ hero, children }: Props) => {
   return (
     <div className='mb-20'>
       <Header />
-      <div className='mt-20 w-full h-[220px]'>{hero}</div>
+      <div className='mt-20 w-full h-[220px] mb-4'>{hero}</div>
       <div className='max-w-7xl m-auto min-h-screen'>
         <div>{children}</div>
       </div>
