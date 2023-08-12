737f6e9a866252f94cac40cabfa9e571c93e2cab
lesgrosman <andreyagasov@gmail.com>
2023-08-12 13:24:54 +0200

    fix: pages queries

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -2,23 +2,28 @@ import { GetStaticPaths, GetStaticProps } from 'next'
 import { MovieDetailResponse, Movies } from '@utils/types'
 import { QueryClient, dehydrate } from '@tanstack/react-query'
 import { QueryKeys } from '@utils/constants'
-import { fetchCredits, fetchDetail, fetchSimilar, fetchVideos } from '@pages/DetailPage/queries'
+import { getCredits, getDetail, getSimilar, getVideos } from '@pages/DetailPage/helpers'
 
 export const getStaticProps: GetStaticProps = async context => {
   const queryClient = new QueryClient()
 
+  const fetchParams = {
+    type: 'movie' as 'movie' | 'tv',
+    id: context.params?.id as string,
+  }
+
   await Promise.all([
     queryClient.prefetchQuery([`${QueryKeys.MOVIE_DETAIL}`, context.params?.id], () =>
-      fetchDetail<MovieDetailResponse>('movie', context.params?.id as string)
+      getDetail<MovieDetailResponse>(fetchParams)
     ),
     queryClient.prefetchQuery([`${QueryKeys.MOVIE_SIMILAR}`, context.params?.id], () =>
-      fetchSimilar<Movies>('movie', context.params?.id as string)
+      getSimilar<Movies>(fetchParams)
     ),
     queryClient.prefetchQuery([`${QueryKeys.MOVIE_CREDITS}`, context.params?.id], () =>
-      fetchCredits('movie', context.params?.id as string)
+      getCredits(fetchParams)
     ),
     queryClient.prefetchQuery([`${QueryKeys.MOVIE_VIDEOS}`, context.params?.id], () =>
-      fetchVideos('movie', context.params?.id as string)
+      getVideos(fetchParams)
     ),
   ])
 
