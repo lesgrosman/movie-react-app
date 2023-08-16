4f06d5d083273d0d045734160f35b1307e4372bf
lesgrosman <andreyagasov@gmail.com>
2023-08-16 20:23:01 +0200

    feat: person page responsive

--- src/pages/PersonPage/BioSection.tsx
+++ src/pages/PersonPage/BioSection.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const BioSection = ({ name, biography }: Props) => (
   <div className='flex flex-col gap-6 border-b-[1px] pb-6'>
-    <h1>{name}</h1>
+    <h1 className='sm:block hidden'>{name}</h1>
     <div>
       <h3>Biography</h3>
       <span className='text-sm'>{biography || '-'}</span>
